9f9751ad97603b08df0822e5be3cb6d050f58bce
jzes <jzes@MacBook-Pro-de-LowCost.local>
2022-05-29 21:12:15 -0300

    renew project, just get the news

--- source/main.py
+++ source/main.py
@@ -1,26 +1,11 @@
-from dotenv import load_dotenv
 from providers.google_searchAPI import GoogleSearchAPI
-from targets.reddit_target import Reddit
-from targets.twitter_target import Twitter
-from service.poster import Poster
-from utils.bgcolors import BGColors
 
 class Main:
 
     def main(self):
-        load_dotenv()
 
         g_search = GoogleSearchAPI()
-        news = g_search.get_news()
-
-        twitter = Twitter()
-        reddit = Reddit()
-        
-        poster = Poster()
-        print(BGColors.OKGREEN+'===Reddit target run==='+BGColors.ENDC)
-        poster.run_poster(reddit, news)
-        print(BGColors.OKGREEN+'===Twitter target run==='+BGColors.ENDC)
-        poster.run_poster(twitter, news)    
+        news = g_search.get_news()   
 
 
 if __name__ == "__main__":
